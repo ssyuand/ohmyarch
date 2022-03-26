2d0ca85288f99913a09cb0211efa9a73873f863e
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-03-26 23:19:36 +0800

    Update neovim.sh

--- script/neovim.sh
+++ script/neovim.sh
@@ -7,8 +7,6 @@ git clone https://github.com/ssyuanmd/neovim ~/.config/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-#mkdir ~/library
-
 ## lua
 #git clone https://github.com/sumneko/lua-language-server ~/library/lua-language-server
 #cd ~/library/lua-language-server
