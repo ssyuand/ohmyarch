2b794b3782560980b17aeea9cd9de8e10d02cf09
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-03-26 18:48:50 +0800

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -50,7 +50,7 @@ arch-chroot /mnt "/ohmyarch/script/chroot.sh"
 ###archiso###
 cp -r ~/ohmyarch /mnt/home/syuan
 sudo chown -R syuan /mnt/home/syuan/ohmyarch
-cd / && umount -R /mnt && clear
+cd / && umount -R /mnt
 echo "done!"
 reboot
 exec bash
