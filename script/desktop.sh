ce88a2c3fa07037a3e2f834b4cf55199ed423d96
ssyuand <ssyuand@gmail.com>
2022-03-26 15:14:08 +0800

    change dotfile to /ohmyarch/dotfile

--- script/desktop.sh
+++ script/desktop.sh
@@ -6,32 +6,29 @@ git clone --recurse-submodules https://github.com/nullgemm/ly.git
 cd ly && sudo make install
 systemctl enable ly
 
-# dotfile
-git clone https://github.com/ssyuanmd/dotfile ~/dotfile
-
 # .bashrc
-cp ~/dotfile/.bashrc ~/
-sudo cp ~/dotfile/.bashrc /root
+cp ~/ohmyarch/dotfile/.bashrc ~/
+sudo cp ~/ohmyarch/dotfile/.bashrc /root
 
 # .config
-cp -r ~/dotfile/.config ~/
-sudo cp -r ~/dotfile/.config /root
+cp -r ~/ohmyarch/dotfile/.config ~/
+sudo cp -r ~/ohmyarch/dotfile/.config /root
 
 # fonts
-sudo cp ~/dotfile/fonts/* /usr/share/fonts/
+sudo cp ~/ohmyarch/dotfile/fonts/* /usr/share/fonts/
 
 # xinitrc
-cp ~/dotfile/.xinitrc ~/
+cp ~/ohmyarch/dotfile/.xinitrc ~/
 
 # dwm
 echo "DWM" | figlet
 git clone https://git.suckless.org/dwm ~/.config/dwm
-cp ~/dotfile/config.h ~/.config/dwm/config.h
+cp ~/ohmyarch/dotfile/config.h ~/.config/dwm/config.h
 cd ~/.config/dwm && sudo make install
 
 #feh
 mkdir ~/Desktop
-tar -xvf ~/dotfile/neon.tar -C ~/Desktop
+tar -xvf ~/ohmyarch/dotfile/neon.tar -C ~/Desktop
 
 echo "Desktop environment done!"
 }
