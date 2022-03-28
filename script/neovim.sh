d07e4b20e19878b4069448e1b11c2c97a145f5e7
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-03-28 09:58:41 +0800

    sudo

--- script/neovim.sh
+++ script/neovim.sh
@@ -2,7 +2,7 @@
 install_neovim() {
 echo "neovim" | figlet
 # packer
-sudo git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 sudo git clone --depth 1 https://github.com/wbthomason/packer.nvim\
   /.local/share/nvim/site/pack/packer/start/packer.nvim
