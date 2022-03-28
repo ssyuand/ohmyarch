061739f1a4c25351c4f1802e346da0574aeaebe6
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-03-28 11:24:39 +0800

    fix lua lsp

--- script/neovim.sh
+++ script/neovim.sh
@@ -11,8 +11,7 @@ sudo git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 install_ls(){
 # lua
-sudo git clone https://github.com/sumneko/lua-language-server 
- ~/library/lua-language-server#cd ~/library/lua-language-server
+sudo git clone https://github.com/sumneko/lua-language-server ~/library/lua-language-server#cd ~/library/lua-language-server
 sudo git submodule update --init --recursive
 cd 3rd/luamake
 ./compile/install.sh
