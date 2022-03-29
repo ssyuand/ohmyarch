6cad43044791d36ffbb4546d05e7c22c09c5e4a9
ssyuand <ssyuand@gmail.com>
2022-03-29 18:28:57 +0800

    tar.xz library

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo chown -R syuan /home/syuan/ohmyarch
+sudo chmod u+x /home/syuan/ohmyarch
+
+DIR=$(pwd)
+. "$DIR/script/dependency.sh"
+. "$DIR/script/desktop.sh"
+. "$DIR/script/neovim.sh"
+
+config_mirror () {
+    PACMAN_MIRROR=$(cat /etc/pacman.d/mirrorlist)
+    if [[ $PACMAN_MIRROR != *"nctu"* ]]; then
+        sudo echo 'Server = https://archlinux.cs.nctu.edu.tw/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+    fi
+	sudo pacman -Sy
+}
+
+# config all shit
+if [[ $1 == "dependency" ]]; then
+	install_dependency
+elif [[ $1 == "desktop" ]]; then
+	install_desktop
+elif [[ $1 == "neovim" ]]; then
+	install_neovim
+elif [[ $1 == "ls" ]]; then
+	install_ls
+else
+	config_mirror
+	install_dependency
+	install_desktop
+	install_neovim
+	startx
+fi
