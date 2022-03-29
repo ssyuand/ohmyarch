9e27f3fd5b6f4a877bc2dfdae00d6f3d16e8fbeb
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-03-29 18:00:53 +0800

    turn aur to a module

--- script/dependency.sh
+++ script/dependency.sh
@@ -37,17 +37,18 @@ for tool in ${tools[@]}; do
 done
 xrandr -s 1920x1080
 
+# fcitx
+printf "export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+" > ~/.xprofile
+}
 
+aur() {
 # AUR
 mkdir ~/library
 echo "AUR" | figlet
 sudo git clone https://aur.archlinux.org/yay.git ~/library/yay
 sudo chown -R syuan ~/library/*
 cd ~/library/yay && makepkg -si --noconfirm
-
-# fcitx
-printf "export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-" > ~/.xprofile
 }
