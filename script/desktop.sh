4e5e27a184aceddd6c27f7a962c014a774f124d3
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-04-01 00:08:11 +0800

    build form source

--- script/desktop.sh
+++ script/desktop.sh
@@ -8,10 +8,6 @@ sudo cp ~/ohmyarch/dotfile/.bashrc /root
 cp -r ~/ohmyarch/dotfile/.config ~/
 sudo cp -r ~/ohmyarch/dotfile/.config /root
 
-# library
-cp ~/ohmyarch/dotfile/library.tar.xz ~/
-cd ~/ && tar -xvf ./library.tar.xz
-
 # fonts
 sudo cp ~/ohmyarch/dotfile/fonts/* /usr/share/fonts/
 
@@ -23,10 +19,12 @@ cp ~/ohmyarch/dotfile/.gitconfig ~/
 
 # dwm
 echo "DWM" | figlet
+git clone https://git.suckless.org/dwm ~/library/dwm
 cp ~/ohmyarch/dotfile/config.h ~/library/dwm/config.h
 cd ~/library/dwm && sudo make install
 
 # picom
+git clone https://github.com/jonaburg/picom ~/library/picom
 cd ~/library/picom && meson --buildtype=release . build
 sudo ninja -C build install
 
@@ -35,6 +33,7 @@ mkdir -p ~/.local/share/rofi/themes
 cp ~/ohmyarch/dotfile/.config/rofi/light.rasi ~/.local/share/rofi/themes/light.rasi
 
 #ly
+git clone --recurse-submodules https://github.com/nullgemm/ly.git ~/library/ly
 cd ~/library/ly && sudo make install
 sudo systemctl enable ly
 
