a6098f816adf95a2baba3a2216e29b27bddf075d
ssyuand <102405111+ssyuand@users.noreply.github.com>
2022-04-13 21:48:01 +0800

    update neovim

--- script/neovim.sh
+++ script/neovim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 install_neovim() {
 echo "neovim" | figlet
+cp -r ~/ohmyarch/dotfile/.config/nvim  ~/.config/nvim
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
